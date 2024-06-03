eb0ada61bc40e691b77b4ee8e2de6007904dbcd7
Vladyslav_Levitskyi <mostly.starfall@gmail.com>
2024-06-04 00:22:20 +0300

    changed settings

--- web_adv_deploy/settings.py
+++ web_adv_deploy/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG") == "True"
+#   DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
