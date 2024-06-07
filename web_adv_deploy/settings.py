6f70a7d40a88704626da8cdcaf6bba8a84456e0b
Vladyslav_Levitskyi <mostly.starfall@gmail.com>
2024-06-07 15:30:31 +0300

    added staticfiles

--- web_adv_deploy/settings.py
+++ web_adv_deploy/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'web_adv_deploy.urls'
@@ -118,11 +119,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICROOT = BASE_DIR/ 'staticfiles'
+
 STATICFILES_DIRS = [
     BASE_DIR/'core_app/static',
     BASE_DIR/'register'
     ]
 
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
