75f4ecb3449fc9a16992bf4757d00941da15ffcb
Vladyslav_Levitskyi <mostly.starfall@gmail.com>
2024-07-31 02:38:03 +0300

    added main.css

--- web_adv_deploy/settings.py
+++ web_adv_deploy/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICROOT = BASE_DIR/ 'staticfiles'
+STATIC_ROOT = BASE_DIR/ 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR/'core_app/static',
