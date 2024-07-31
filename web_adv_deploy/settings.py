4ba06e1f221db572c829acbf7206b096a8682c31
Vladyslav_Levitskyi <mostly.starfall@gmail.com>
2024-07-31 14:40:27 +0300

    solved problem with server Error 500

--- web_adv_deploy/settings.py
+++ web_adv_deploy/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 OPENWEATHER_KEY = os.environ.get("OPENWEATHER_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "True") == "False"
 ALLOWED_HOSTS = ["web-adv-deploy.onrender.com", "127.0.0.1"]
 #   DEBUG = os.getenv("DEBUG", "False") == "True"
 #   ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(',')
